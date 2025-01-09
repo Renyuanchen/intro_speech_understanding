4554ab2bf28fd18634435e4bc3879ef0e1f79b0d
Renyuanchen <ryc401304@gmail.com>
2025-01-09 19:57:38 +0900

    Update homework9.py

--- 2024_fall/lec09/homework9.py
+++ 2024_fall/lec09/homework9.py
@@ -16,5 +16,18 @@ def fourier_synthesis(num_harmonics, X, T0):
     
     x[n] = (2/N) * sum_{l=1}^{num_harmonics} |X[l*N//T0]| * cos(2*pi*l*n/T0 + angle(X[l*N//T0]))
     '''
-    raise RuntimeError("You need to change this part")
-
+    N = len(X)
+    x = np.zeros(N)
+    
+    # Perform Fourier synthesis
+    for n in range(N):
+        summation = 0
+        for l in range(1, num_harmonics + 1):
+            idx = l * N // T0  # The corresponding index in the Fourier transform
+            magnitude = np.abs(X[idx])
+            phase = np.angle(X[idx])
+            summation += magnitude * np.cos(2 * np.pi * l * n / T0 + phase)
+        
+        x[n] = (2 / N) * summation
+    
+    return x
